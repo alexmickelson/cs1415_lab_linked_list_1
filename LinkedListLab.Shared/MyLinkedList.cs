f30640a681a3f07dc878a1dc98e64e9604711e98
Alex Mickelson <alex.mickelson@snow.edu>
2023-10-09 10:55:26 -0600

    basic function interface scaffolded

---
+++ LinkedListLab.Shared/MyLinkedList.cs
@@ -0,0 +1,41 @@
+public class MyLinkedList
+{
+  private MyNode? head { get; set; }
+
+  public int Count
+  {
+    get
+    {
+      return 0;
+    }
+  }
+  public string DataAtIndex(int index)
+  {
+    return "";
+  }
+
+  public void Append(string newData)
+  {
+    return;
+  }
+
+  public string Pop()
+  {
+    return "";
+  }
+
+
+  // // Extra Credit
+  // public void AddAt(int index, string newData )
+  // {
+  //   // add node to the middle of the list
+  //   return;
+  // }
+
+  // // Extra Credit
+  // public string RemoveAt(int index)
+  // {
+  //   // remove node from list, return data from removed node
+  //   return "";
+  // }
+}
