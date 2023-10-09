67d663dad7ea5722bcc266c47e143bed59dd05ac
Alex Mickelson <alex.mickelson@snow.edu>
2023-10-09 10:57:24 -0600

    added main

--- LinkedListLab.Console/Program.cs
+++ LinkedListLab.Console/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+var linkedList = new MyLinkedList();
+
+linkedList.Append("this is the first string");
+linkedList.Append("second string");
+linkedList.Append("I am the third string");
+
+
+
+Console.WriteLine("Here is the linked list data:");
+for (int i = 0; i < linkedList.Count; i++)
+{
+  Console.WriteLine($"{i}: {linkedList.DataAtIndex(i)}");
+}
